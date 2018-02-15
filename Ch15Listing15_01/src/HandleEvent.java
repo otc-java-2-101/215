0030cef6650fd1eefbaefbcb0b85d02f1f50dead
Unknown <ro0504008@otc.edu>
2018-02-15 13:02:50 -0600

    0

---
+++ Ch15Listing15_01/src/HandleEvent.java
@@ -0,0 +1,70 @@
+import javafx.application.Application;
+import javafx. geometry.Pos;
+import javafx. scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+class Type {
+
+    Type() {
+        
+    }
+}
+
+public class HandleEvent extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        HBox pane = new HBox(10);
+        pane.setAlignment(Pos.CENTER);
+        Button btOK = new Button("OK");
+        Button btCancel = new Button("Cancel");
+        
+        OKHandlerClass handler1=new OKHandlerClass();
+        btOK.setOnAction(handler1);
+        
+        CancelHandlerClass handler2=new CancelHandlerClass();
+        btCancel.setOnAction(handler2);
+        
+        
+        pane.getChildren().addAll(btOK, btCancel);
+        
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("HandleEvent");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+}
+
+
+class OKHandlerClass implements EventHandler<ActionEvent>{
+    @Override
+    public void handle (ActionEvent e){
+        System.out.println("OK button clicked");
+    }
+    
+    
+    
+}
+class CancelHandlerClass implements EventHandler<ActionEvent>{
+    @Override
+    public void handle (ActionEvent e){
+        System.out.println("Cancel button clicked");
+    }
+    
+    
+    
+}
+
+
---
+++ Ch15Listing15_04/src/AnonymousHandlerDemo.java
@@ -0,0 +1,72 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class AnonymousHandlerDemo extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        Text text = new Text(40, 40, "programing is fun");
+        Pane pane = new Pane(text);
+        Button btUp = new Button("Up");
+        Button btDown = new Button("down");
+        Button btLeft = new Button("Left");
+        Button btRight = new Button("Right");
+
+        
+        HBox hBox = new HBox(btUp, btDown, btLeft, btRight);
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        
+        BorderPane borderPane = new BorderPane(pane);
+        borderPane.setBottom(hBox);
+        
+        btUp.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle (ActionEvent e){
+                text.setY(text.getY()>10? text.getY()-5 : 10);
+            } 
+        });
+        btDown.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle (ActionEvent e){
+                text.setY(text.getY()<pane.getHeight()? text.getY()+5 : pane.getHeight());
+            } 
+        });
+                
+        btLeft.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle (ActionEvent e){
+                text.setX(text.getX() > 0 ? text.getX()-5 : 0);
+            } 
+        });
+        btRight.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle (ActionEvent e){
+                text.setX(text.getX()<pane.getWidth()-100? text.getX()+5 : pane.getWidth()-100);
+            } 
+        });
+        
+        
+        // Create a scene and place it in the stage
+    Scene scene = new Scene(borderPane, 400, 350);
+    primaryStage.setTitle("AnonymousHandlerDemo"); // Set title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+    }
+    
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+    
+}
